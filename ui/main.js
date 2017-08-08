3e696108d6aadf4bfebed5a1666a1675532583b8
crp987654321 <crprajapati111@gmail.com>
2017-08-08 17:45:55 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="New Value";
 //move the image
-var img=document.getElementById('madi');
+var img=document.getElementById('madi.png');
 img.onclick=function(){
   img.style.marginLeft="100px";  
 };
