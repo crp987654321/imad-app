4277e3216b13c516c21bee50670947db67515989
crp987654321 <crprajapati111@gmail.com>
2017-08-09 16:09:05 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,14 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML="New Value";
-//move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveright(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function () {
-  var interval= setInterval(moveright,50);
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onlcick = function (){
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in a correct span
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
